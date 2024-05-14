f4a9f12f99752d0ba7c93a534cfea47ad006352e
David Kizivat <david.kizivat@gmail.com>
2024-05-14 14:00:58 +0200

    refactor: run prettier on root level files as well

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,48 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier',
+	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: ['.svelte'],
 	},
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: {
-          // Specify a parser for each lang.
-          ts: '@typescript-eslint/parser',
-          js: 'espree',
-          typescript: '@typescript-eslint/parser'
-        }
-      }
-    }
-  ],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: {
+					// Specify a parser for each lang.
+					ts: '@typescript-eslint/parser',
+					js: 'espree',
+					typescript: '@typescript-eslint/parser',
+				},
+			},
+		},
+	],
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
-  rules: {
-    'no-undef': 'off',
-    // no-undef has been turned off because of this:
-    // basically, it causes issues and TS does those checks so it's redundant
-    // https://typescript-eslint.io/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_'
-      }
-    ]
-  }
+	rules: {
+		'no-undef': 'off',
+		// no-undef has been turned off because of this:
+		// basically, it causes issues and TS does those checks so it's redundant
+		// https://typescript-eslint.io/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				varsIgnorePattern: '^_',
+				argsIgnorePattern: '^_',
+			},
+		],
+	},
 };
