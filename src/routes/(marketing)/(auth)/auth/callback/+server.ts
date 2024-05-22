42b69f0895fe7266d957875858d15287f73938f8
David Kizivat <david.kizivat@gmail.com>
2024-05-22 17:41:46 +0200

    chore: add error message where needed

--- src/routes/(marketing)/(auth)/auth/callback/+server.ts
+++ src/routes/(marketing)/(auth)/auth/callback/+server.ts
@@ -11,6 +11,7 @@ export const GET = async (event) => {
 	if (code) {
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
+			console.error(error);
 			throw redirect(303, `/${next.slice(1)}`);
 		}
 	}
